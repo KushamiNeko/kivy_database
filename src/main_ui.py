567c1b79c6364503cae57489b317615769f320d0
Kushami Neko <missing10630@gmail.com>
2016-10-29 01:18:58 +0800

    start building selectable data row

--- src/main_ui.py
+++ src/main_ui.py
@@ -42,18 +42,16 @@ from kivy.properties import StringProperty
 import pymongo
 from mongo_interface import MongoInterface
 
-db_connection = ('localhost', 27017)
-db_collections = 'tifana_db_test'
-
 
 class CustomLabel(Label):
     bg_color = ListProperty([0.15, 0.15, 0.15, 1])
 
     def __init__(self, bg_color=None, **kargs):
-        super(CustomLabel, self).__init__(**kargs)
 
         if bg_color:
             self.bg_color = bg_color
+
+        super(CustomLabel, self).__init__(**kargs)
 
 
 class DataPopUp(Popup):
@@ -131,12 +129,13 @@ class DataPopUpRow(BoxLayout):
     field_data = StringProperty('')
 
     def __init__(self, field_name, field_data, **kargs):
-        super(DataPopUpRow, self).__init__(**kargs)
 
         self.field_name = field_name
 
         if field_data:
             self.field_data = field_data
+
+        super(DataPopUpRow, self).__init__(**kargs)
 
     def get_data(self):
         return self.ids['data_input'].text
@@ -158,23 +157,105 @@ class ConfirmationPopUp(Popup):
         super(ConfirmationPopUp, self).dismiss()
 
 
+class CategoryAreaRow(BoxLayout):
+    pass
+
+
+class DataAreaRow(BoxLayout):
+
+    # layout = BoxLayout(id="data_row_{}".format(str(i)),
+    #                    orientation='horizontal',
+    #                    size_hint_x=1,
+    #                    size_hint_y=1,
+    #                    spacing=2)
+
+    def __init__(self, parent, categories, datas, **kargs):
+        super(DataAreaRow, self).__init__(**kargs)
+
+        self.orientation = 'horizontal'
+        self.size_hint = (1, 1)
+        self.spacing = 2
+
+        self.data_labels = []
+
+        for j in range(0, len(categories)):
+
+            if datas:
+                try:
+                    data = datas[categories[j]]
+
+                    if data:
+                        text = data
+                    else:
+                        text = ''
+
+                except (IndexError, KeyError):
+                    text = ''
+
+                label = CustomLabel(text=text)
+
+            else:
+                label = CustomLabel(text='')
+
+            self.add_widget(label)
+            self.data_labels.append(label)
+
+        parent.add_widget(self)
+
+    def set_data(self, categories, datas):
+
+        for j in range(0, len(categories)):
+
+            text = ''
+
+            if datas:
+                try:
+                    data = datas[categories[j]]
+
+                    if data:
+                        text = data
+
+                except (IndexError, KeyError):
+                    pass
+
+            self.data_labels[j].text = text
+
+
 class ManagementWindow(BoxLayout):
 
-    client = pymongo.MongoClient(db_connection[0], db_connection[1])
-    db = client[db_collections]
+    #client = pymongo.MongoClient(db_connection[0], db_connection[1])
+    #db = client[db_collections]
 
-    db_table = MongoInterface(db, "tables")
+    #db_table = MongoInterface(db, "tables")
 
-    tables = sorted([i['name'] for i in db_table.find_data()])
+    #tables = sorted([i['name'] for i in db_table.find_data()])
+    tables = ListProperty()
 
-    rows_per_page = 15
+    #rows_per_page = 15
 
-    def __init__(self, **kargs):
-        super(ManagementWindow, self).__init__(**kargs)
+    def __init__(self, client_db,  **kargs):
 
-        self.categories_len = 0
+        self.client_db = client_db
+
+        self.db_table = MongoInterface(client_db, "tables")
+
+        self.tables = sorted([i['name'] for i in self.db_table.find_data()])
 
         self.current_collection = self.tables[0]
+
+        self.categories_len = 0
+        self.collection_datas_len = 0
+
+        self.rows_per_page = 15
+
+        self.data_rows = []
+
+        # call super class init method after initializing all the
+        # variable we need
+
+        super(ManagementWindow, self).__init__(**kargs)
+
+        # build widget after calling init method of super class
 
         self.update_widget_data(collection=self.current_collection)
 
@@ -205,39 +286,55 @@ class ManagementWindow(BoxLayout):
                 label = ''
 
             table_label = CustomLabel(id="category_row_{}".format(str(i)),
-                                      text=str(label), bg_color=[0.1, 0.25, 0.4, 1])
+                                      text=str(label),
+                                      bg_color=[0.1, 0.25, 0.4, 1])
+
             self.ids['category_row'].add_widget(table_label)
 
-        db_customer = MongoInterface(self.db, collection)
-        customers = db_customer.find_data()
+        db_collection = MongoInterface(self.client_db, collection)
+        collection_datas = db_collection.find_data()
+
+        self.collection_datas_len = collection_datas.count()
 
         for i in range(0, self.rows_per_page):
-            layout = BoxLayout(id="data_row_{}".format(str(i)),
-                               orientation='horizontal',
-                               size_hint_x=1,
-                               size_hint_y=1,
-                               spacing=2)
 
-            for j in range(0, self.categories_len):
-                try:
-                    customer = customers[i]
-                    label = CustomLabel(id="{}_{}".format(
-                        str(i), self.collection_categories[j]),
-                        text=str(customer[self.collection_categories[j]]))
+            try:
+                datas = collection_datas[i]
+            except IndexError:
+                datas = None
 
-                except (IndexError, KeyError):
-                    label = CustomLabel(text='')
+            data_row = DataAreaRow(self.ids['data_area'],
+                                   self.collection_categories,
+                                   datas)
 
-                layout.add_widget(label)
+            self.data_rows.append(data_row)
 
-            self.ids['data_area'].add_widget(layout)
+            # layout = BoxLayout(id="data_row_{}".format(str(i)),
+            #                   orientation='horizontal',
+            #                   size_hint_x=1,
+            #                   size_hint_y=1,
+            #                   spacing=2)
+
+            # for j in range(0, self.categories_len):
+            #    try:
+            #        customer = customers[i]
+            #        label = CustomLabel(id="{}_{}".format(
+            #            str(i), self.collection_categories[j]),
+            #            text=str(customer[self.collection_categories[j]]))
+
+            #    except (IndexError, KeyError):
+            #        label = CustomLabel(text='')
+
+            #    layout.add_widget(label)
+
+            # self.ids['data_area'].add_widget(layout)
 
     def update_data(self, row=None, field=None):
         pass
 
     def show_data_popup(self, title, operation):
-        p = DataPopUp(title, self, 'add', self.db, self.current_collection,
-                      self.collection_categories)
+        p = DataPopUp(title, self, 'add', self.client_db,
+                      self.current_collection, self.collection_categories)
         p.open()
 
     def show_confirmation_popup(self):
@@ -247,12 +344,24 @@ class ManagementWindow(BoxLayout):
 
 class ManagementApp(App):
 
+    db_connection = ('localhost', 27017)
+    db_collections = 'tifana_db_test'
+
+    def __init__(self, **kargs):
+
+        self.client = pymongo.MongoClient(
+            self.db_connection[0],
+            self.db_connection[1])
+
+        self.db = self.client[self.db_collections]
+
+        super(ManagementApp, self).__init__(**kargs)
+
     def build(self):
-        self.program = ManagementWindow()
-        return self.program
+        return ManagementWindow(self.db)
 
     def end(self):
-        self.program.close_db()
+        self.client.close()
 
 
 if __name__ == '__main__':
